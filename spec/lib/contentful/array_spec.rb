2f0d5f182a059debe4daa32f34b8ee40fc5fe261
Guilherme Cattani <gcattani1@gmail.com>
2023-06-01 01:29:36 +0200

    Lint fixes

--- spec/lib/contentful/array_spec.rb
+++ spec/lib/contentful/array_spec.rb
@@ -9,9 +9,9 @@ RSpec.describe Contentful::Array, type: :model do
   describe "new" do
     context "when successful" do
       context "when lazy loaded" do
-        it 'creates a simple new array of given model', :vcr do
+        it "creates a simple new array of given model", :vcr do
           array = Contentful::Array.new(Contentful.new({ "content_type": "recipe" }).entries, Recipe, lazy_loaded: true)
-  
+
           expect(array.items).not_to be_empty
           expect(array.count).not_to be_zero
           expect(array.items.first.lazy_loaded).to eq(true)
@@ -21,9 +21,9 @@ RSpec.describe Contentful::Array, type: :model do
       end
 
       context "when not lazy loaded" do
-        it 'creates a new array with depth of given model', :vcr do
+        it "creates a new array with depth of given model", :vcr do
           array = Contentful::Array.new(Contentful.new({ "content_type": "recipe" }).entries, Recipe, lazy_loaded: false)
-  
+
           expect(array.items).not_to be_empty
           expect(array.count).not_to be_zero
           expect(array.items.first.lazy_loaded).to eq(false)
@@ -32,18 +32,18 @@ RSpec.describe Contentful::Array, type: :model do
         end
       end
     end
-    
-    context 'when errored' do
-      describe 'invalid status' do
+
+    context "when errored" do
+      describe "invalid status" do
         before do
           allow(response_double).to receive(:success?).and_return(false)
         end
-        it 'raises InvalidStatus' do
+        it "raises InvalidStatus" do
           expect { Contentful::Array.new(response_double, Recipe, lazy_loaded: false) }.to raise_error(Contentful::Errors::InvalidStatus)
         end
       end
-      describe 'invalid body' do
-        it 'raises InvalidBody', :vcr do
+      describe "invalid body" do
+        it "raises InvalidBody", :vcr do
           expect { Contentful::Array.new(Contentful.new.entry("NysGB8obcaQWmq0aQ6qkC"), Recipe, lazy_loaded: false) }.to raise_error(Contentful::Errors::InvalidBody)
         end
       end
